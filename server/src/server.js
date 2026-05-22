f007cf7d57e64074425df6eeb200fbbcd90818ea
Ganesh Parajuli <parajuliganesh10@gmail.com>
2026-05-22 22:10:33 +0545

    Updated server.js

--- server/src/server.js
+++ server/src/server.js
@@ -7,4 +7,53 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`ACCIOCALL API is running on port ${PORT}`)
 
+});require("dotenv").config();
+
+const http = require("http");
+
+const { Server } = require("socket.io");
+
+const app = require("./app");
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*"
+  }
+});
+
+
+// SOCKET CONNECTION
+io.on("connection", (socket) => {
+
+  console.log("User connected:", socket.id);
+
+  // join room
+  socket.on("join-room", (roomName) => {
+
+    socket.join(roomName);
+
+    console.log(`${socket.id} joined ${roomName}`);
+
+    // notify everyone in room
+    io.to(roomName).emit(
+      "user-joined",
+      `${socket.id} joined room`
+    );
+  });
+
+  // disconnect
+  socket.on("disconnect", () => {
+
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+
+  console.log(`Server running on port ${PORT}`);
 });
